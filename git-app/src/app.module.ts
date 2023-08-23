1054432c9c4ef103334fb5365ce13c8bf3b8bd6b
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-23 14:17:51 -0400

    test-pr

--- git-app/src/app.module.ts
+++ git-app/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-// import { GithubWebhooksModule } from '@dev-thought/nestjs-github-webhooks';
+import { GithubWebhooksModule } from '@dev-thought/nestjs-github-webhooks';
 import { OctokitModule } from 'nestjs-octokit';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    // GithubWebhooksModule.forRootAsync({
-    //   useFactory: () => ({ webhookSecret: 'nest' }),
-    // }),
+    GithubWebhooksModule.forRootAsync({
+      useFactory: () => ({ webhookSecret: 'nest' }),
+    }),
     OctokitModule.forRoot({
       isGlobal: true,
       octokitOptions: {
