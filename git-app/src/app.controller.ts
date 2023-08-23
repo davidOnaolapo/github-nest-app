75b80547939e01a4ae28487487975cd09c88f927
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-23 10:17:54 -0400

    test

--- git-app/src/app.controller.ts
+++ git-app/src/app.controller.ts
@@ -14,7 +14,7 @@ export class AppController {
 
   @UseGuards(GithubGuard)
   @GithubWebhookEvents(['push', 'pullrequest'])
-  @Post('2A0CKbN8OjqZtrA')
+  @Post('event_handler')
   async onPullRequest(@Body() payload: any) {
     //inside webhook, grab pr id/ add label
     console.log('**PAYLOAD**', payload);
