14d7c7bebe28fb00cf43aa4373700a69e104fa9c
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-24 14:42:56 -0400

    test-pr

--- git-app/src/app.controller.ts
+++ git-app/src/app.controller.ts
@@ -18,10 +18,10 @@ export class AppController {
   async onPullRequest(@Body() payload: any) {
     //inside webhook, grab pr id/ add label
     console.log('**PAYLOAD*', payload);
-    // const response = await this.octokitService.rest.search.repos({
-    //   q: 'nest-js',
-    // });
-    // return response.data.items;
+    const response = await this.octokitService.rest.search.repos({
+      q: 'nest-js',
+    });
+    return response.data.items;
   }
 
   @Post('onPushToMaster')
