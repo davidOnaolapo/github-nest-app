fb3c3773d8c29836481bafec9ad0acd6cf585b71
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-28 13:37:20 -0400

    test

--- git-app/src/app.service.ts
+++ git-app/src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
   private graphqlWithAuth: any; // You can define a type here
 
   constructor() {
+    console.log('**hmm**', process.env.GITHUB_WEBHOOK_TOKEN);
     this.graphqlWithAuth = graphql.defaults({
       headers: {
         authorization: process.env.GITHUB_WEBHOOK_TOKEN, // Replace with your token
