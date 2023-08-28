4f498dd8f4e671d8117fa120e5defcd283650da3
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-28 14:35:31 -0400

    test

--- git-app/src/app.service.ts
+++ git-app/src/app.service.ts
@@ -13,15 +13,15 @@ export class AppService {
     });
   }
 
-  async getRepositoryIssues(owner: string, repo: string, branch: string) {
+  async getRepositoryIssues(owner: string, _repo: string, branch: string) {
     const query = `
     query ($owner: String!, $repo: String!, $branch: String!) {
-        repository(owner: ${owner}, name: ${repo}) {
-          workflowRuns(first: 10, status: "in_progress", branch: ${branch}) {
+        repository(owner: $owner, name: $repo) {
+          workflowRuns(first: 10, status: "in_progress", branch: $branch) {
             nodes {
               id
               name
-              workflow: workflow {
+              workflow {
                 name
               }
             }
