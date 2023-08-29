71fbcc6d225fd8d39a2a7ca3b23f9aa0a508b1e9
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-08-29 02:03:01 -0400

    test

--- git-app/src/app.controller.ts
+++ git-app/src/app.controller.ts
@@ -40,7 +40,7 @@ export class AppController {
 
   @Post('onPushToMaster')
   async onPushToMaster(@Body() payload: any) {
-    console.log('*PAYLOAD**', payload);
+    console.log('**PAYLOAD**', payload);
     return;
   }
 
