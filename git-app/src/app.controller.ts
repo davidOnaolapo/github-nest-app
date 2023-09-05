e3ad117149e5156cd047a49279119cc176796682
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-09-05 13:00:12 -0400

    test

--- git-app/src/app.controller.ts
+++ git-app/src/app.controller.ts
@@ -13,51 +13,55 @@ export class AppController {
   @GithubWebhookEvents(['check_suite', 'pull_request'])
   @Post('onPrWorkflowEvent')
   async onCheckSuite(@Body() payload: any) {
-    if (payload.pull_request) {
-      console.log(
-        '***PR COMMIT WORKFLOWS ABOUT TO RUN ***',
-        payload.pull_request.id,
-      );
-    }
+    // if (payload.pull_request) {
+    //   console.log(
+    //     '***PR COMMIT WORKFLOWS ABOUT TO RUN ***',
+    //     payload.pull_request.id,
+    //   );
+    // }
 
     if (payload.check_suite) {
-      console.log('***A WORKFLOWW STATE CHANGED***');
-      const workflowInfo =
-        await this.githubGraphqlService.getRepositoryWorkflowInfo(
-          payload.repository.owner.login,
-          payload.repository.name,
-        );
-      console.log('***WORKFLOWINFO***');
-      workflowInfo.forEach((theWorkFlow: any) => {
-        console.log(
-          theWorkFlow.workflowRun?.workflow,
-          theWorkFlow.workflowRun?.checkSuite,
-        );
-      });
-      const hasFailure = workflowInfo.some((theWorkflow: any) => {
-        return theWorkflow.workflowRun?.checkSuite?.conclusion === 'FAILURE';
-      });
-      const allWorkflowsDone = workflowInfo.every((theWorkflow: any) => {
-        return (
-          theWorkflow.workflowRun?.checkSuite?.status === 'COMPLETED' ||
-          theWorkflow.workflowRun?.checkSuite?.status === undefined
-        );
-      });
-      let statusForRepo: string;
-      allWorkflowsDone && !hasFailure
-        ? (statusForRepo = 'success')
-        : hasFailure
-        ? (statusForRepo = 'failure')
-        : (statusForRepo = 'pending'),
-        console.log('**STATUSFORREPO**', statusForRepo);
-      await this.githubGraphqlService.updatePrMergeability(
-        payload.repository.owner.login,
-        payload.repository.name,
-        payload.check_suite.pull_requests[0].head.sha,
-        statusForRepo,
+      console.log(
+        '***A WORKFLOWW STATE CHANGED***',
+        payload.check_suite.pull_requests[0].head,
       );
+      //   const workflowInfo =
+      //     await this.githubGraphqlService.getRepositoryWorkflowInfo(
+      //       payload.repository.owner.login,
+      //       payload.repository.name,
+      //     );
+      //   console.log('***WORKFLOWINFO***');
+      //   workflowInfo.forEach((theWorkFlow: any) => {
+      //     console.log(
+      //       theWorkFlow.workflowRun?.workflow,
+      //       theWorkFlow.workflowRun?.checkSuite,
+      //     );
+      //   });
+      //   const hasFailure = workflowInfo.some((theWorkflow: any) => {
+      //     return theWorkflow.workflowRun?.checkSuite?.conclusion === 'FAILURE';
+      //   });
+      //   const allWorkflowsDone = workflowInfo.every((theWorkflow: any) => {
+      //     return (
+      //       theWorkflow.workflowRun?.checkSuite?.status === 'COMPLETED' ||
+      //       theWorkflow.workflowRun?.checkSuite?.status === undefined
+      //     );
+      //   });
+      //   let statusForRepo: string;
+      //   allWorkflowsDone && !hasFailure
+      //     ? (statusForRepo = 'success')
+      //     : hasFailure
+      //     ? (statusForRepo = 'failure')
+      //     : (statusForRepo = 'pending'),
+      //     console.log('**STATUSFORREPO**', statusForRepo);
+      //   await this.githubGraphqlService.updatePrMergeability(
+      //     payload.repository.owner.login,
+      //     payload.repository.name,
+      //     payload.check_suite.pull_requests[0].head.sha,
+      //     statusForRepo,
+      //   );
+      // }
+      // return;
     }
-    return;
   }
 }
 
