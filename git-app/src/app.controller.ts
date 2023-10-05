8ec944177c605fb99525a3f8f15106db0a6432e1
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-10-05 15:01:11 -0400

    get status

--- git-app/src/app.controller.ts
+++ git-app/src/app.controller.ts
@@ -15,7 +15,7 @@ export class AppController {
   async onCheckSuite(@Body() payload: any) {
     if (payload.pull_request) {
       console.log(
-        '****PR COMMIT WORKFLOWS ABOUT TO RUN **',
+        '*****PR COMMIT WORKFLOWS ABOUT TO RUN **',
         payload.pull_request.id,
       );
     }
